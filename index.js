78710749ba5dd79aa8edd0244e1b3985d6e1032b
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-01 14:36:55 +0300

    index.html updated

---
+++ index.js
@@ -0,0 +1,3 @@
+const images = ["https://cdn.getyourguide.com/img/tour/7c16cf602517a89b749c71ace374f81f06954329038bd1c911c4e4770c4ef542.jpg/98.jpg", 
+                "https://content.r9cdn.net/rimg/dimg/cf/47/e7a46c58-city-30961-16779fb3c85.jpg?width=1366&height=768&xhint=1519&yhint=1077&crop=true"];
+                "https://content.r9cdn.net/rimg/dimg/cf/47/e7a46c58-city-30961-16779fb3c85.jpg?width=1366&height=768&xhint=1519&yhint=1077&crop=true"
