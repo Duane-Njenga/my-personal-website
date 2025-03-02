26e8b2f45c687a562a4fa12e67c89c1e10fe0554
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-02 17:33:49 +0300

    Added to css and JS (Darkmode)

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-const images = ["https://cdn.getyourguide.com/img/tour/7c16cf602517a89b749c71ace374f81f06954329038bd1c911c4e4770c4ef542.jpg/98.jpg", 
-                "https://content.r9cdn.net/rimg/dimg/cf/47/e7a46c58-city-30961-16779fb3c85.jpg?width=1366&height=768&xhint=1519&yhint=1077&crop=true"];
-                "https://content.r9cdn.net/rimg/dimg/cf/47/e7a46c58-city-30961-16779fb3c85.jpg?width=1366&height=768&xhint=1519&yhint=1077&crop=true"
+document.getElementById("newsletter-form").addEventListener("submit", function(event) {
+     event.preventDefault(); 
+
+    let emailInput = document.getElementById("email");
+    let message = document.getElementById("message");
+
+    if (validateEmail(emailInput.value)) {
+        message.style.color = "green";
+        message.textContent = "Thank you for subscribing!";
+        emailInput.value = ""; 
+    } else {
+        message.style.color = "red";
+        message.textContent = "Please enter a valid email address.";
+    }
+});
+
+function validateEmail(email) {
+    return email.includes("@") && email.includes(".") && email.indexOf("@") < email.lastIndexOf(".");
+}
+
+
+const darkModeToggle = document.getElementById("darkmodeToggle");
+const body = document.body;
+
+darkModeToggle.addEventListener("change", function () {
+    if (darkModeToggle.checked) {
+        body.classList.add("dark-mode");
+;
+    } else {
+        body.classList.remove("dark-mode");
+    }
+});
